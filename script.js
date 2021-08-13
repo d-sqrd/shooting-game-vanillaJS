62b577b31b16a757043c3de282a3ac5d052f524f
d-sqrd <deb.bunny98@gmail.com>
2021-08-13 20:20:32 +0530

    Commented the file

--- script.js
+++ script.js
@@ -93,13 +93,18 @@ const player1 = new Player(cw /2, ch / 2, 30, 'dodgerblue');
 let enemyArr = [];
 // let paritcleArr = [];
 let projectileArr = [];
+
+// function to initialize every array and the window UI
 function init(){
     projectileArr = [];
     enemyArr = [];
     // paritcleArr = [];
     scoreElement.innerHTML = 0;
 }
+
 function spawnEnemy(){
+
+    // spawn an enemy every 1000 milliseconds
     setInterval(() => {
         const radius = Math.random() * 20 + 8;
         let x;
@@ -111,7 +116,7 @@ function spawnEnemy(){
             x = Math.random() * cw;
             y = Math.random() <= 0.5 ? 0 - radius : ch + radius;
         }
-        const angle = Math.atan2(ch / 2 - y, cw / 2 - x);
+        const angle = Math.atan2(ch / 2 - y, cw / 2 - x);   // calculate the angle between the spawned enemy and the player
         const velocity = { 
             x: Math.cos(angle),
             y: Math.sin(angle)
@@ -120,11 +125,12 @@ function spawnEnemy(){
         enemyArr.push(new Enemy(x, y, velocity, radius, color));
     },1000);
 }
+
 let score = 0;
 function animate(){
     let animationId = requestAnimationFrame(animate);
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-    ctx.fillRect(0, 0, cw, ch);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';   // alpha = 0.1 results in fading trails
+    ctx.fillRect(0, 0, cw, ch);     // clear the canvas window with black color in each animation frame
     player1.draw();
 
     // paritcleArr.forEach((particle, particleIndex) => {
@@ -137,6 +143,8 @@ function animate(){
 
     projectileArr.forEach((projectile, projectileIndex) => {
         projectile.update();
+
+        // if condition to remove out of screen projectiles from projectile array
         if(projectile.x + projectile.radius < 0 || 
             projectile.x - projectile.radius > cw ||
             projectile.y + projectile.radius < 0 || 
@@ -146,9 +154,11 @@ function animate(){
             },0);
         }
     });
+
+    // looping through each enemy
     enemyArr.forEach((enemy, enemyIndex) => {
         enemy.update();
-        const dist = Math.hypot(player1.x - enemy.x, player1.y - enemy.y);
+        const dist = Math.hypot(player1.x - enemy.x, player1.y - enemy.y);  // calculate the distance between the player and the current enemy
 
         // when enemy hits player
         if(dist - player1.radius - enemy.radius < 1){
@@ -157,10 +167,11 @@ function animate(){
             scoreCtr.innerHTML = score;
         }
 
+        // looping through each projectile
         projectileArr.forEach((projectile, projectileIndex) => {
-            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);    // calculate distance between current enemy and current projectile
             
-            //when projectile touches enemy
+            // when projectile touches enemy
             if( dist - projectile.radius - enemy.radius < 1){
                 score += 100;
                 scoreElement.innerHTML = score;
@@ -172,6 +183,7 @@ function animate(){
                 //     }, Math.random() * 3, enemy.color));
                 // }
                 
+                // if enemy is of large radius shrink it after projectile hits it else remove the enemy from the enemy array
                 if(enemy.radius - 10 > 10){
                     gsap.to(enemy, {
                         radius: enemy.radius - 10
@@ -190,7 +202,7 @@ function animate(){
     });
 }
 window.addEventListener('click',function(event){
-    const angle = Math.atan2(event.y - ch / 2, event.x - cw / 2);
+    const angle = Math.atan2(event.y - ch / 2, event.x - cw / 2);   // calculate the direction in which projectile will travel
     const velocity = { 
         x: Math.cos(angle) * 4,
         y: Math.sin(angle) * 4
